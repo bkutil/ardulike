66b4a1e60eec1d77992306b9643b02fe03efd1e2
Balazs Kutil <balazs@kutilovi.cz>
2014-11-06 13:36:44 +0100

    Toggle keypress event only if the value changed since last 50ms

--- ardulike/ardulike.ino
+++ ardulike/ardulike.ino
@@ -1,6 +1,8 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+int last_change_at  = 0;
+int last_value      = 1024;
 
 void setup() {
   Serial.begin(115200);
@@ -9,12 +11,18 @@ void setup() {
 
 void loop() {
   int reading = analogRead(A0);
+  int now = millis();
+
   lcd.clear();
   lcd.setCursor(0, 1);
 
-  if (reading == 407) {
-    lcd.print("LEFT");
-    Serial.println("LEFT");
+  if (reading != last_value && (now - last_change_at) > 50) {
+    if (reading == 407) {
+      lcd.print("LEFT");
+      Serial.println("LEFT");
+    }
+    last_value = reading;
+    last_change_at = now;
   } else {
     Serial.println("NONE");
   }
