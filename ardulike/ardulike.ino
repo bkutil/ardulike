1e7fc828e6ad654ecb9564ae6df9741f493e9cc2
Balazs Kutil <balazs@kutilovi.cz>
2014-11-06 13:57:55 +0100

    Make hero walk around, do not redraw if the env did not change

--- ardulike/ardulike.ino
+++ ardulike/ardulike.ino
@@ -1,31 +1,48 @@
 #include <LiquidCrystal.h>
 
+enum directions { none, left, right };
+
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
 int last_change_at  = 0;
 int last_value      = 1024;
+int position        = 0;
 
 void setup() {
-  Serial.begin(115200);
   lcd.begin(16, 2);
+  lcd.setCursor(position, 1);
+  lcd.print('@');
 }
 
 void loop() {
+  directions direction = none;
+
   int reading = analogRead(A0);
   int now = millis();
 
-  lcd.clear();
-  lcd.setCursor(0, 1);
-
   if (reading != last_value && (now - last_change_at) > 50) {
     if (reading == 407) {
-      lcd.print("LEFT");
-      Serial.println("LEFT");
+      direction = left;
+    } else
+    if (reading == 0) {
+      direction = right;
     }
     last_value = reading;
     last_change_at = now;
-  } else {
-    Serial.println("NONE");
   }
 
-  delay(20);
+  if (direction == left) {
+    position = (16 + position - 1) % 16;
+  } else
+  if (direction == right) {
+    position = (16 + position + 1) % 16;
+  }
+
+  if (direction != none) {
+    lcd.clear();
+    lcd.setCursor(position, 1);
+    lcd.print('@');
+  }
+
+  delay(30);
 }
