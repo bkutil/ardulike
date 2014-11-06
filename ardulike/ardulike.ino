d0e6b196b935562acff56c75ee594b0cbaf9c2c6
Balazs Kutil <balazs@kutilovi.cz>
2014-11-06 17:03:51 +0100

    Move the input reading into a separate class, use it in the main loop

--- ardulike/ardulike.ino
+++ ardulike/ardulike.ino
@@ -1,12 +1,9 @@
 #include <LiquidCrystal.h>
-
-enum directions { none, left, right };
+#include "Input.h"
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-int last_change_at  = 0;
-int last_value      = 1024;
-int position        = 0;
+uint32_t position        = 0;
 
 void setup() {
   lcd.begin(16, 2);
@@ -15,32 +12,21 @@ void setup() {
 }
 
 void loop() {
-  directions direction = none;
+  bool changed = false;
+  Button input = Input::get();
 
-  int reading = analogRead(A0);
-  int now = millis();
-
-  if (reading != last_value && (now - last_change_at) > 50) {
-    if (reading == 407) {
-      direction = left;
-    } else
-    if (reading == 0) {
-      direction = right;
-    }
-    last_value = reading;
-    last_change_at = now;
-  }
-
-  if (direction == left) {
-    position = (16 + position - 1) % 16;
+  if (input == ButtonLeft) {
+    position -= 1;
+    changed   = true;
   } else
-  if (direction == right) {
-    position = (16 + position + 1) % 16;
+  if (input == ButtonRight) {
+    position += 1;
+    changed   = true;
   }
 
-  if (direction != none) {
+  if (changed) {
     lcd.clear();
-    lcd.setCursor(position, 1);
+    lcd.setCursor(position % 16, 1);
     lcd.print('@');
   }
 
