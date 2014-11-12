98dd54120cc27f9fc9eb9b0906ed2601d174aa48
Balazs Kutil <balazs@kutilovi.cz>
2014-11-12 23:31:01 +0100

    Display level and screen (for debugging)

--- ardulike/ardulike.ino
+++ ardulike/ardulike.ino
@@ -3,10 +3,17 @@
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-uint32_t position        = 0;
+uint8_t position        = 0;
+uint8_t level           = 0;
 
 void setup() {
   lcd.begin(16, 2);
+
+  lcd.setCursor(0,0);
+  lcd.print("L:");
+  lcd.print(level);
+  lcd.print("S:");
+  lcd.print(position / 16);
   lcd.setCursor(position, 1);
   lcd.print('@');
 }
@@ -26,6 +33,12 @@ void loop() {
 
   if (changed) {
     lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("L:");
+    lcd.print(level);
+    lcd.print("S:");
+    lcd.print(position / 16);
+
     lcd.setCursor(position % 16, 1);
     lcd.print('@');
   }
