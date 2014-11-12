a6a16c8586b6a5c6f9814ade6a70922fed385769
Balazs Kutil <balazs@kutilovi.cz>
2014-11-12 23:34:06 +0100

    Move redraws into separate functions

--- ardulike/ardulike.ino
+++ ardulike/ardulike.ino
@@ -6,16 +6,23 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 uint8_t position        = 0;
 uint8_t level           = 0;
 
-void setup() {
-  lcd.begin(16, 2);
+void draw_player() {
+  lcd.setCursor(position % 16, 1);
+  lcd.print('@');
+};
 
+void draw_position() {
   lcd.setCursor(0,0);
   lcd.print("L:");
   lcd.print(level);
   lcd.print("S:");
   lcd.print(position / 16);
-  lcd.setCursor(position, 1);
-  lcd.print('@');
+}
+
+void setup() {
+  lcd.begin(16, 2);
+  draw_position();
+  draw_player();
 }
 
 void loop() {
@@ -33,14 +40,8 @@ void loop() {
 
   if (changed) {
     lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("L:");
-    lcd.print(level);
-    lcd.print("S:");
-    lcd.print(position / 16);
-
-    lcd.setCursor(position % 16, 1);
-    lcd.print('@');
+    draw_position();
+    draw_player();
   }
 
   delay(30);
