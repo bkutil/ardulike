02184ddecda10e02929731b209e961a33f2d1865
Balazs Kutil <balazs@kutilovi.cz>
2014-11-17 22:35:54 +0100

    Instead of directly accessing lcd, use char[], render once

--- ardulike/ardulike.ino
+++ ardulike/ardulike.ino
@@ -8,25 +8,25 @@ uint8_t level           = 0;
 
 bool init_draw          = true;
 
+char lcd_t_row[17];
+char lcd_b_row[17];
+
 void draw_player() {
-  lcd.setCursor(position % 16, 1);
-  lcd.print('@');
+  lcd_b_row[position % 16] = '@';
 };
 
 void draw_position() {
-  lcd.setCursor(0,0);
-  lcd.print("L:");
-  lcd.print(level);
-  lcd.print("S:");
-  lcd.print(position / 16);
+  snprintf(lcd_t_row, 17, "L:%dS:%d", level, position / 16);
 }
 
 void draw_terrain() {
   randomSeed(((position / 16 + 1) << 8) + level);
 
   for (uint8_t i = 0; i < 16; i++) {
-    if ((random() % 4) == 0) { lcd.setCursor(i, 1); lcd.print("T"); }
+    lcd_b_row[i] = ((random() % 4) == 0) ? 'T' : ' ';
   }
+
+  lcd_b_row[16] = '\0';
 };
 
 void draw_stairs() {
@@ -38,17 +38,20 @@ void draw_stairs() {
   while (up == down) { down = random(256); }
 
   if (level > 0 && up >= (position / 16) * 16 && up < ((position / 16) + 1) * 16) {
-    lcd.setCursor(up % 16, 1);
-    lcd.print("<");
+    lcd_b_row[up % 16] = '<';
   }
 
   if (level < 255 && down >= (position / 16) * 16 && down < ((position / 16) + 1) * 16) {
-    lcd.setCursor(down % 16, 1);
-    lcd.print(">");
+    lcd_b_row[down % 16] = '>';
   }
 }
 
 void draw() {
+  draw_position();
+  draw_terrain();
+  draw_stairs();
+  draw_player();
+
   if (init_draw) {
     lcd.begin(16, 2);
     init_draw = false;
@@ -56,10 +59,10 @@ void draw() {
     lcd.clear();
   }
 
-  draw_position();
-  draw_terrain();
-  draw_stairs();
-  draw_player();
+  lcd.setCursor(0, 0);
+  lcd.print(lcd_t_row);
+  lcd.setCursor(0, 1);
+  lcd.print(lcd_b_row);
 };
 
 void setup() {
